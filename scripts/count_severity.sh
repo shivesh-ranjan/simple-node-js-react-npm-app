2d5a439e2bca5fd3767081248afebabd9b1cbcdc
shivesh-ranjan <ranjanshivesh71@gmail.com>
2025-02-25 10:02:46 +0000

    changing trivy image scan failure policy

--- scripts/count_severity.sh
+++ scripts/count_severity.sh
@@ -4,13 +4,15 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 json_file="$1"
+critical_count="$2"
+high_count="$3"
 if [ ! -f "$json_file" ]; then
 	echo "File not found: $json_file"
 	exit 1
 fi
 while IFS= read -r line; do
-	if echo "$line" | jq -e '.severity == "CRITICAL" and .count > 0' > /dev/null || echo "$line" | jq -e '.severity == "HIGH" and .count > 0' > /dev/null; then
-		echo "Found CRITICAL or HIGH severity vulnerability..."
+	if echo "$line" | jq -e ".severity == 'CRITICAL' and .count > $critical_count" > /dev/null || echo "$line" | jq -e ".severity == 'HIGH' and .count > $high_count" > /dev/null; then
+		echo "Found more CRITICAL or HIGH severity vulnerabilities than allowed in policy"
 		exit 1
 	fi
 done < "$json_file"
