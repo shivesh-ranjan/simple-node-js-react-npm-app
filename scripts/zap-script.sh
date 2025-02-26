beef8f077ee3ef7c2be55cfda3636a4ff0eb789c
shivesh-ranjan <ranjanshivesh71@gmail.com>
2025-02-26 06:10:06 +0000

    changing zap script to fail for testing...

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,11 +1,14 @@
-docker run -u root --name zap -v $(pwd)/zap:/zap/wrk:rw --network="host" zaproxy/zap-stable zap-full-scan.py -t http://localhost:3000 -r zap-scan-report.html
-if [ $? == 1 ] || [ $? == 3 ]
-then
-    docker stop mynodeapp
-    docker rm mynodeapp
-    exit 1
-else
-    docker stop mynodeapp
-    docker rm mynodeapp
-    exit 0
-fi
+# docker run -u root --name zap -v $(pwd)/zap:/zap/wrk:rw --network="host" zaproxy/zap-stable zap-full-scan.py -t http://localhost:3000 -r zap-scan-report.html
+# if [ $? == 1 ] || [ $? == 3 ]
+# then
+#     docker stop mynodeapp
+#     docker rm mynodeapp
+#     exit 1
+# else
+#     docker stop mynodeapp
+#     docker rm mynodeapp
+#     exit 0
+# fi
+docker stop mynodeapp
+docker rm mynodeapp
+exit 1
