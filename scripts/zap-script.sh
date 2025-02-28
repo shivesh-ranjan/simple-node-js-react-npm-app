f2d35e89a323f61e64ce0a2c2d595a0928e6c1bd
shivesh-ranjan <ranjanshivesh71@gmail.com>
2025-02-28 07:19:01 +0000

    deleting html report while being inside the zap container...

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,4 +1,4 @@
-docker run -u root --name zap -v $(pwd)/zap:/zap/wrk:rw --network="host" zaproxy/zap-stable sleep 9999999999999 #zap-full-scan.py -t http://localhost:3000 -r zap-scan-report.html
+docker run -d -u root --name zap -v $(pwd)/zap:/zap/wrk:rw --network="host" zaproxy/zap-stable sleep 9999999999999 #zap-full-scan.py -t http://localhost:3000 -r zap-scan-report.html
 docker exec zap zap-full-scan.py -t http://localhost:3000 -r zap-scan-report.html
 if [ $? == 1 ] || [ $? == 3 ]
 then
